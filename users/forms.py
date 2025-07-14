3f8dc0c6b28a5d31c38437d6e281df73719293d7
Md Mazharul Islam Antor <mazharulantor2015@gmail.com>
2025-07-15 02:12:11 +1000

    chore(auth): complete Django built-in login and update registration form

--- users/forms.py
+++ users/forms.py
@@ -1,24 +1,20 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import CustomUser
+
 
 class CustomUserRegistrationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = [
-            'username',
-            'first_name',
-            'last_name',
-            'email',
-            'phone_number',
-            'address',
-            'occupation',
-            'password1',
-            'password2',
-        ]
+        fields = ('username', 'first_name', 'last_name', 'email', 'phone_number', 'address', 'occupation')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['phone_number'].required = True
-        self.fields['address'].required = True
-        self.fields['occupation'].required = True
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+class CustomLoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,11 @@
+from django.contrib.auth.views import LoginView
 from django.urls import path
+
+from .forms import CustomLoginForm
 from .views import register
 
 urlpatterns = [
     path('register/', register, name='register'),
+    path('login/', LoginView.as_view(template_name='users/login.html', authentication_form=CustomLoginForm),
+         name='login'),
 ]
